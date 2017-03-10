3ac00c68e2d2282f036c4bd309c63c0b3b94853d
juancamiloespana <camilo1260@gmail.com>
2017-03-09 22:59:15 -0500

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -3,6 +3,7 @@ fechas<-data$Date
 fechas<-as.Date(fechas,"%d/%m/%Y")
 data$Date<-fechas
 data_filtered<-subset(data,Date == '2007-02-01' | Date == '2007-02-02')
+par(lwd=480,lheight=480)
 with(data_filtered,hist(Global_active_power,col="red",main="Global Active Power",xlab="Gloabl Active Power (Kilowatts)"))
-dev.copy(png,file="plot1.png")
+dev.copy(png,file="plot1.png",width=480,height=480)
 dev.off()
