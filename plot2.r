551bf7b9c35704fa825c5dbf9064ecbd6bbfe297
juancamiloespana <camilo1260@gmail.com>
2017-03-12 12:06:58 -0500

    Update plot2.r

--- plot2.r
+++ plot2.r
@@ -4,7 +4,7 @@ data$Date<-as.Date(data$Date,"%d/%m/%Y")
 data$fechas<-fechas
 data_filtered<-subset(data,Date == '2007-02-01' | Date == '2007-02-02')
 data_filtered$time_m <- strptime(paste(data_filtered$fechas, data_filtered$Time), "%d/%m/%Y %H:%M:%S")
-plot(data_filtered$Time,data_filtered$Global_active_power)
+
 plot(x = data_filtered$time_m, y = data_filtered$Global_active_power,
      ylab = "Global Active Power (kilowatts)",  type="l", xlab = "")
 axis(1, at=1:3, lab=c("Thu","Fri", "Sat"))
